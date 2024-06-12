ebe85f91110ed7b4f5961ff2fcad02f60a625efc
Madhuri98k <madhuri98.korapaka@gmail.com>
2024-06-12 12:15:58 +0530

    adding listitems by TagName

---
+++ GIT-TUTORIAL/Task3/listItems.js
@@ -0,0 +1,7 @@
+const listItems= document.getElementsByTagName("li");
+for(let i=0;i<listItems.length;i++)
+  {
+     listItems[4].style.color="blue";
+listItems[i].style.fontStyle="italic";
+    
+}
