ca0931b4d94340991a1d075eddff98f1fd227f2a
Madhuri98k <madhuri98.korapaka@gmail.com>
2024-06-12 11:44:13 +0530

    task 2 question added

---
+++ GIT-TUTORIAL/TASK2/question2.js
@@ -0,0 +1,9 @@
+const fruit=document.getElementsByClassName('fruit');
+for(let i=0;i<fruit.length;i++)
+  {
+    fruit[2].style.background='yellow';
+  
+fruit[i].style.fontWeight="bold";
+    }
+    
+    
--- GIT-TUTORIAL/demo.js
+++ GIT-TUTORIAL/demo.js
@@ -1,1 +1,14 @@
-console.log('hi...this is demo ')
+//console.log('hi...this is demo ')//
+const Header=getElementById('header');
+Header.textContent='Fruit World';
+Header.style.fontColor='Orange';
+Header.style.background='Green';
+Header.style.borderBottom=" 3px solid orange";
+
+const MainHeading=getElementById('main-heading');
+MainHeading.style.fontColor='green';
+
+
+const vote=getElementById("thanks");
+vote.innerHTML=<p>please visit us again.</p>;
+
