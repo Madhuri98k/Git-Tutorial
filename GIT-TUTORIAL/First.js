d0c964f2031a4acf32cd43bb02264c290b74a486
Madhuri98k <madhuri98.korapaka@gmail.com>
2024-06-12 10:20:03 +0530

    add first

---
+++ GIT-TUTORIAL/First.js
@@ -0,0 +1,15 @@
+const MainHeading = document.getElementById('main-heading');
+MainHeading.textContent="Fruit World";
+MainHeading.style.color="orange";
+
+const Header=document.getElementById('header');
+Header.style.background='green';
+Header.style.borderBottom='3px solid orange';
+
+const BasketHeading = document.getElementById('basket-heading');
+  BasketHeading.style.color='green';
+
+
+const thanks=document.getElementById('thanks');
+thanks.innerHTML ="<p>Please visit us again</p>";
+
